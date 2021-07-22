910f685e5b40bcc60ed4662218b69abe4e2268a4
Guilherme Henrique <gui_lirasilva@outlook.com>
2021-07-21 23:23:47 -0300

    Implementando a seleção de lugares

--- src/main/java/br/com/caelum/ingresso/model/Ingresso.java
+++ src/main/java/br/com/caelum/ingresso/model/Ingresso.java
@@ -3,16 +3,63 @@ package br.com.caelum.ingresso.model;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
 import br.com.caelum.ingresso.model.descontos.Desconto;
 
+@Entity
 public class Ingresso {
-
+	
+	@Id
+	@GeneratedValue(strategy	=	GenerationType.IDENTITY)
+	private	Integer	id;
+	@ManyToOne
 	private Sessao sessao;
+	@ManyToOne
+	private Lugar lugar;
 	private BigDecimal preco;
+	
+	@Enumerated(EnumType.STRING)
+	private	TipoDeIngresso	tipoDeIngresso;
+	
+	/**
+	*	@deprecated	hibernate	only
+	*/
+	public	Ingresso(){
+	}
 
 	public Ingresso(Sessao sessao, Desconto tipoDeDesconto) {
 		this.sessao = sessao;
 		this.preco = tipoDeDesconto.aplicarDescontoSobre(sessao.getPreco());
+	}
+
+	public Ingresso(Sessao sessao, TipoDeIngresso tipoDeIngresso, Lugar lugar) {
+		this.sessao = sessao;
+		this.tipoDeIngresso = tipoDeIngresso;
+		this.preco = this.tipoDeIngresso.aplicaDesconto(sessao.getPreco());
+		this.lugar = lugar;
+	}
+
+	public Lugar getLugar() {
+		return lugar;
+	}
+
+	public void setLugar(Lugar lugar) {
+		this.lugar = lugar;
+	}
+
+	public TipoDeIngresso getTipoDeIngresso() {
+		return tipoDeIngresso;
+	}
+
+	public void setTipoDeIngresso(TipoDeIngresso tipoDeIngresso) {
+		this.tipoDeIngresso = tipoDeIngresso;
 	}
 
 	public BigDecimal getPreco() {
@@ -30,6 +77,5 @@ public class Ingresso {
 	public void setSessao(Sessao sessao) {
 		this.sessao = sessao;
 	}
-	
-	
+
 }
--- src/main/java/br/com/caelum/ingresso/model/descontos/DescontoParaEstudantes.java
+++ src/main/java/br/com/caelum/ingresso/model/descontos/DescontoParaEstudantes.java
@@ -6,9 +6,14 @@ public class DescontoParaEstudantes implements Desconto {
 
 	@Override
 	public BigDecimal aplicarDescontoSobre(BigDecimal precoOriginal) {
-		
-		return	precoOriginal.divide(new	BigDecimal("2.0"));
 
+		return precoOriginal.divide(new BigDecimal("2.0"));
+
+	}
+
+	@Override
+	public String getDescricao() {
+		return "Desconto Estudante";
 	}
 
 }

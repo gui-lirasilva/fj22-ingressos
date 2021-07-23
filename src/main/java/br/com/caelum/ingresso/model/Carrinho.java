6f06187a0f6e1db56b87a0cb85516cb84676046f
Guilherme Henrique <gui_lirasilva@outlook.com>
2021-07-22 23:52:49 -0300

    Implementando a compra

--- src/main/java/br/com/caelum/ingresso/model/Carrinho.java
+++ src/main/java/br/com/caelum/ingresso/model/Carrinho.java
@@ -33,4 +33,11 @@ public class Carrinho {
 		return ingressos.stream().map(Ingresso::getPreco).reduce(BigDecimal::add).orElse(BigDecimal.ZERO);
 	}
 
+	public Compra toCompra() {
+		return new Compra(ingressos);
+	}
+
+	public void limpa() {
+		this.ingressos.clear();
+	}
 }
